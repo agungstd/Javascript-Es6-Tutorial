75417e2b066b13f9a96515f3f74988ea1612c464
agungstd <apakatadunia11@gmail.com>
2025-03-25 08:08:47 +0700

    Add styling options and examples of using display functions

--- 03-modul/js/lib.js
+++ 03-modul/js/lib.js
@@ -1,19 +1,32 @@
 function display(message) {
   const el = document.createElement("div");
   el.innerText = message;
+  el.classList.add('display-message');
   document.body.appendChild(el);
 }
+
 function display2(message) {
   const el = document.createElement("div");
   el.innerText = message;
+  el.classList.add('display-message-2');
   document.body.appendChild(el);
 }
 
-export { display, display2 };
+// Added a new function with styling options
+function displayStyled(message, options = {}) {
+  const el = document.createElement("div");
+  el.innerText = message;
+  
+  // Apply custom styling if provided
+  el.style.color = options.color || 'black';
+  el.style.backgroundColor = options.backgroundColor || 'transparent';
+  el.style.padding = options.padding || '5px';
+  el.style.margin = options.margin || '2px';
+  el.style.fontWeight = options.fontWeight || 'normal';
+  el.classList.add('display-message-styled');
 
-// const disp = {
-//   display: display,
-//   display2: display2,
-// };
+  document.body.appendChild(el);
+  return el;
+}
 
-// export default disp;
+export { display, display2, displayStyled };
--- 03-modul/js/script.js
+++ 03-modul/js/script.js
@@ -1,6 +1,18 @@
-import { display, display2 } from "./lib.js";
-
-// import disp from "./lib.js";
+import { display, display2, displayStyled } from "./lib.js";
 
 display("Hello World!");
 display2("Hello World2!");
+
+// Additional examples to showcase new functionality
+displayStyled("Styled Message 1", { 
+  color: 'white', 
+  backgroundColor: 'blue',
+  padding: '10px',
+  fontWeight: 'bold'
+});
+
+displayStyled("Styled Message 2", { 
+  color: 'green', 
+  backgroundColor: 'lightyellow',
+  margin: '5px'
+});

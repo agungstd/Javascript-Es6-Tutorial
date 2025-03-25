f0a03c4ce3ffab08695eaa10018257cb993733d6
agungstd <apakatadunia11@gmail.com>
2025-03-25 08:11:28 +0700

    add an advanced example of computed properties

--- 04-class/js/computed.js
+++ 04-class/js/computed.js
@@ -21,7 +21,36 @@ class Person {
   get [nama]() {
     return `${this.firstName} ${this.lastName}`;
   }
+
+  // New method using computed property method
+  static [Symbol.hasInstance](instance) {
+    return instance.firstName !== undefined && instance.lastName !== undefined;
+  }
+
+  // New computed method for greeting
+  ['getGreeting']() {
+    return `Hello, ${this.fullName}!`;
+  }
 }
 
 let person2 = new Person("Pojok", "Code");
 console.log(person2.fullName);
+console.log(person2.getGreeting());
+
+// Additional example of computed property with dynamic key
+const dynamicKey = 'status';
+const user = {
+  [`${dynamicKey}Code`]: 200,
+  [`is${dynamicKey.charAt(0).toUpperCase() + dynamicKey.slice(1)}`]: true
+};
+
+console.log(user.statusCode);
+console.log(user.isStatus);
+
+// Demonstrating computed property with Symbol
+const uniqueSymbol = Symbol('description');
+const objectWithSymbol = {
+  [uniqueSymbol]: 'This is a symbol-keyed property'
+};
+
+console.log(objectWithSymbol[uniqueSymbol]);

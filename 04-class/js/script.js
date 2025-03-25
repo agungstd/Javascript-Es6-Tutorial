9df507a872db6b458edddbf397adcf95958a5b9e
agungstd <apakatadunia11@gmail.com>
2025-03-25 08:20:16 +0700

    Add detailed demonstration of class and constructor characteristics

--- 04-class/js/script.js
+++ 04-class/js/script.js
@@ -1,5 +1,4 @@
 // contoh cara membuat objek ES5
-
 function Person(name) {
   this.name = name;
 }
@@ -33,3 +32,54 @@ console.log(code instanceof Object);
 // 1. pendeklarasian class tidak hoisted seperti funcrion
 // 2. semua code dalam class dijalankan dengan mode strict dan ini tidak dapat diubah
 // 3. kerika membuat objek di class harus dengan new
+
+// Demonstrasi perbedaan class dan function constructor
+console.log("Demonstrasi karakteristik class:");
+
+try {
+  // Contoh tidak hoisting
+  // Uncommenting line below will cause ReferenceError
+  // let TestHoisting = new ClassExample();
+
+  class ClassExample {
+    constructor() {
+      this.type = "class";
+    }
+  }
+
+  // Fungsi constructor dapat dipanggil sebelum deklarasi (hoisting)
+  function FunctionExample() {
+    this.type = "function";
+  }
+
+  console.log("Class characteristics test:");
+  
+  // Demonstrasi keharusan menggunakan 'new'
+  try {
+    // Ini akan menghasilkan TypeError
+    // Person2("Tanpa new"); 
+  } catch (error) {
+    console.log("Error saat memanggil class tanpa new:", error.message);
+  }
+
+  // Menambahkan static method
+  class StaticMethodDemo {
+    constructor(name) {
+      this.name = name;
+    }
+
+    static createAnonymous() {
+      return new StaticMethodDemo("Anonymous");
+    }
+
+    greet() {
+      return `Hello, ${this.name}!`;
+    }
+  }
+
+  const anonymousPerson = StaticMethodDemo.createAnonymous();
+  console.log(anonymousPerson.greet());
+
+} catch (error) {
+  console.error("Terjadi kesalahan:", error.message);
+}
